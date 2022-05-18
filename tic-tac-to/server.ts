3af6ca8ab360e514dbb0f68e5ca0c67b69d899d2
hanochshachar <goralot550@gmail.com>
2022-05-18 12:03:54 +0300

    kk

--- tic-tac-to/server.ts
+++ tic-tac-to/server.ts
@@ -1,3 +1,14 @@
 
+const express = require('express');
+const app = express();
+const port = process.env.PORT | 4000;
 
+app.use(express.json());
+app.use(express.static('public'));
+
+
+
+app.listen(port, ()=>{
+    console.log(`listen on port ${port}`)
+})
 
