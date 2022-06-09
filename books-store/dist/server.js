8466a3fa2a58bacef50005d9bb72b1f1071f1021
hanochshachar <goralot550@gmail.com>
2022-06-10 01:31:58 +0300

    ל

--- books-store/dist/server.js
+++ books-store/dist/server.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 exports.__esModule = true;
 var express = require('express');
 var app = express();
-var port = process.env.PORT | 5000;
+var port = process.env.PORT | 3000;
 app.use(express.static('public'));
 app.use(express.json());
 var bookRoute_1 = __importDefault(require("./route/bookRoute"));
