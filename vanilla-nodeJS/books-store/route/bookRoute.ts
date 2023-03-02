d3867d312d65b5aa6642ff81dddcbd429188bd24
hanochshachar <goralot550@gmail.com>
2023-03-02 22:43:50 +0200

    upload project

---
+++ vanilla-nodeJS/books-store/route/bookRoute.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+const router = express.Router();
+
+import { postBook, getBook, updateDescription, updatePrice, deleteBook, clientGet, clientCartPost, cartGet, deleteBookCart } from '../controlers/bookCont';
+
+
+router.post('/booksStore', postBook)
+        .get('/booksStore', getBook)
+        .put('/updateDesc', updateDescription)
+        .put('/updatePrice', updatePrice)
+        .delete('/deleteBook', deleteBook)
+        .get('/clientGet', clientGet)
+        .post('/clientCart', clientCartPost)
+        .get('/clientCart', cartGet)
+        .delete('/deleteCart', deleteBookCart)
+
+export default router;
